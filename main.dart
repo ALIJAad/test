2f9094e235e126b019272cb49fd3c35a8e0b232b
Ali <you@gmail.com>
2022-03-28 00:18:47 +0500

    hah

---
+++ main.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+
+void main() {
+  // question one IS DONE
+  // int factorial = 1;
+  // int nume = 5;
+
+  // for (var i = 2; i <= nume; i++) {
+  //   factorial = factorial * i;
+  //   print(factorial);
+  // print(result);
+  // }
+
+  // // question 3 is done
+  // var testList = [22, 2, 22, 4];
+  // print(testList[0] + testList[1] + testList[2] + testList[3]);
+
+  // question 6
+
+  // print('Type your mystey word');
+
+  // var pa = stdin.readLineSync();
+  // print(pa.runtimeType);
+}
